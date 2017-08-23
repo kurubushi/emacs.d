0435524b5f3c888e122e75006393b4b79ebfa085
kurubushi <krbshi@gmail.com>
2017-08-23 17:21:52 +0900

    add: emacs.d

---
+++ init.d/standerd.el
@@ -0,0 +1,24 @@
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+; standerd.el
+;----------
+; write standerd configures in it.
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+; markdown
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(use-package markdown-mode
+  :install
+  (el-get-bundle markdown-mode))
+
+
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+; haskell
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(use-package haskell-mode
+  :install
+  (el-get-bundle haskell-mode))
