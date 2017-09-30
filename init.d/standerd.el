6deb72dfc0389689df8941bef5a0cd63ea315943
kurubushi <krbshi@gmail.com>
2017-10-01 05:02:28 +0900

    add: config for ocaml in emacs
    merlin，tuareg を追加した

--- init.d/standerd.el
+++ init.d/standerd.el
@@ -60,3 +60,34 @@
 (use-package haskell-mode
   :install
   (el-get-bundle haskell-mode))
+
+
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+; ocaml
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(use-package tuareg-mode
+  :install
+  (el-get-bundle tuareg-mode)
+  :config
+  (add-hook 'tuareg-mode-hook 'tuareg-imenu-set-imenu)
+  (setq auto-mode-alist
+    (append '(("\\.ml[ily]?$" . tuareg-mode)
+              ("\\.topml$" . tuareg-mode))
+            auto-mode-alist))
+  (autoload 'utop-setup-ocaml-buffer "utop" "Toplevel for OCaml" t)
+  (add-hook 'tuareg-mode-hook 'utop-setup-ocaml-buffer))
+
+(use-package merlin
+  :install
+  (el-get-bundle merlin
+    :type github
+    :pkgname "ocaml/merlin"
+    :load-path "emacs")
+  :config
+  (add-hook 'tuareg-mode-hook 'merlin-mode)
+  (setq merlin-use-auto-complete-mode t)
+  (setq merlin-error-after-save nil))
+  
