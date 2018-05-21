61dac9ea02d08e61328068bffab1762e6a0cf73f
kurubushi <krbshi@gmail.com>
2018-05-22 03:29:25 +0900

    [emacs] fix a part of evil-collection
    閉じ括弧からの対応のとりかたの関数が間違ってたので修正．

--- init.d/core.el
+++ init.d/core.el
@@ -210,7 +210,49 @@
 (el-get-bundle evil-collection)
 (use-package evil-collection
   :after evil
-  :config (evil-collection-init))
+  :config
+  (evil-collection-init)
+
+  ;; https://github.com/emacs-evil/evil-collection/issues/79
+  ;; commited code was wrong.
+  ;; this is called by (evil-collection-paren-setup) 
+  (defun evil-collection-paren-show-paren-function (f &rest args)
+    "Integrate `show-paren-function' with `evil'."
+    (if (not (bound-and-true-p evil-mode))
+        (apply f args)
+      (if (if (memq 'not evil-highlight-closing-paren-at-point-states)
+              (memq evil-state evil-highlight-closing-paren-at-point-states)
+            (not (memq evil-state evil-highlight-closing-paren-at-point-states)))
+          (apply f args)
+        (let ((pos (point)) syntax narrow)
+          (setq pos
+                (catch 'end
+                  (dotimes (var (1+ (* 2 evil-show-paren-range)))
+                    (if (zerop (mod var 2))
+                        (setq pos (+ pos var))
+                      (setq pos (- pos var)))
+                    (setq syntax (syntax-class (syntax-after pos)))
+                    (cond
+                     ((eq syntax 4)
+                      (setq narrow pos)
+                      (throw 'end pos))
+                     ((eq syntax 5)
+  ;;                    (throw 'end (1+ pos))))))) ; wrong line
+                      (throw 'end pos)))))) ; this is correct
+          (if pos
+              (save-excursion
+                (goto-char pos)
+                (save-restriction
+                  (when narrow
+                    (narrow-to-region narrow (point-max)))
+                  (apply f args)))
+            ;; prevent the preceding pair from being highlighted
+            (dolist (ov '(show-paren--overlay
+                          show-paren--overlay-1
+                          show-paren-overlay
+                          show-paren-overlay-1))
+              (let ((ov (and (boundp ov) (symbol-value ov))))
+                (when (overlayp ov) (delete-overlay ov))))))))))
 
 (defun evil-define-key-escape-spc (keymaps)
   (general-define-key :states 'normal
