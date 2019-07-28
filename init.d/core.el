710ebc3eb92e521dbeb6163e685fc317748c3cf7
kurubushi <krbshi@gmail.com>
2019-07-29 03:43:34 +0900

    [emacs] neotree

--- init.d/core.el
+++ init.d/core.el
@@ -166,6 +166,13 @@ http://emacsredux.com/blog/2013/06/21/eval-and-replace/"
 (add-hook 'after-make-frame-functions #'set-my-font-atonce) ; systemd 経由だと適用されない．しょうがないので hook する
 
 
+;; emoji
+(use-package all-the-icons
+  :el-get all-the-icons
+  :config
+  (all-the-icons-install-fonts t))
+
+
 ;; theme
 (use-package doom-themes
   :el-get doom-themes
@@ -477,6 +484,7 @@ http://emacsredux.com/blog/2013/06/21/eval-and-replace/"
 
 
 
+
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; shell
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -486,5 +494,40 @@ http://emacsredux.com/blog/2013/06/21/eval-and-replace/"
   :general
   (general-define-key :keymaps '(normal insert visual emacs)
                       "<f8>" 'shell-pop))
+
+
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; neotree
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(use-package neotree
+  :el-get neotree
+  :commands (neotree-show neotree-hide neotree-dir neotree-find)
+  :custom
+  (neo-theme 'nerd2)
+  :config
+  (evil-set-initial-state 'neotree-mode 'emacs)
+  (defun neotree-toggle-on-current-buffer ()
+    "Open neotree buffer with the current buffer."
+    (interactive)
+    (if (neo-global--window-exists-p)
+        (neotree-hide)
+      (let ((filepath (buffer-file-name)))
+        (neotree-find filepath))))
+  (defun neotree-go-up-node ()
+    "Go to parent of selected node."
+    (interactive)
+    (goto-char (point-min))
+    (neotree-select-up-node))
+  :general
+  (general-define-key :keymaps '(normal insert visual emacs)
+                      "<f9>" 'neotree-toggle-on-current-buffer)
+  (general-define-key :keymaps 'neotree-mode-map
+                      "j" 'neotree-next-line
+                      "k" 'neotree-previous-line
+                      "l" 'neotree-change-root
+                      "h" 'neotree-go-up-node))
 
 ;;; core.el ends here
