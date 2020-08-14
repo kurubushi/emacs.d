267263ea4ed759cd1dcdae9753cc5fd21478a965
kurubushi <krbshi@gmail.com>
2020-08-14 19:24:57 +0900

    [emacs] persp-mode => perspeen

--- init.d/core.el
+++ init.d/core.el
@@ -553,21 +553,69 @@ http://emacsredux.com/blog/2013/06/21/eval-and-replace/"
 ;; persp-mode
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(use-package persp-mode
-  :el-get persp-mode
+;;(use-package persp-mode
+;;  :el-get persp-mode
+;;  :config
+;;  (persp-mode 1)
+;;  (defun persp-helm-mini ()
+;;    (interactive)
+;;    (with-persp-buffer-list () (helm-mini)))
+;;  :general
+;;  (general-define-key :keymaps '(normal)
+;;                      :prefix "SPC"
+;;                      "ps" 'persp-frame-switch
+;;                      "pS" 'persp-window
+;;                      "pr" 'persp-rename
+;;                      "pc" 'persp-copy
+;;                      "pk" 'persp-kill
+;;                      "pp" 'persp-helm-mini))
+
+
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+; perspeen
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(use-package perspeen
+  :el-get perspeen
+  :init
+  (setq perspeen-use-tab t)
   :config
-  (persp-mode 1)
-  (defun persp-helm-mini ()
-    (interactive)
-    (with-persp-buffer-list () (helm-mini)))
-  :general
+  (perspeen-mode)
   (general-define-key :keymaps '(normal)
                       :prefix "SPC"
-                      "ps" 'persp-frame-switch
-                      "pS" 'persp-window
-                      "pr" 'persp-rename
-                      "pc" 'persp-copy
-                      "pk" 'persp-kill
-                      "pp" 'persp-helm-mini))
+                      "pC" 'perspeen-create-ws
+                      "pN" 'perspeen-next-ws
+                      "pP" 'perspeen-previous-ws
+                      "pK" 'perspeen-delete-ws
+                      "pR" 'perspeen-rename-ws
+                      "pD" 'perspeen-change-root-dir
+                      "p'" 'perspeen-goto-last-ws
+                      "p1" 'perspeen-ws-jump
+                      "p2" 'perspeen-ws-jump
+                      "p3" 'perspeen-ws-jump
+                      "p4" 'perspeen-ws-jump
+                      "p5" 'perspeen-ws-jump
+                      "p6" 'perspeen-ws-jump
+                      "p7" 'perspeen-ws-jump
+                      "p8" 'perspeen-ws-jump
+                      "p9" 'perspeen-ws-jump
+                      "pe" 'perspeen-ws-eshell
+                      "pc" 'perspeen-tab-create-tab
+                      "pk" 'perspeen-tab-del
+                      "pn" 'perspeen-tab-next
+                      "pp" 'perspeen-tab-prev)
+  ; By default,
+  ; perspeen-tab--header-line-inactive <- mode-line-active
+  ; perspeen-tab--powerline-inactive1 <- mode-line-active + grayscale
+  (set-face-attribute 'perspeen-tab--header-line-inactive nil
+                      :background "gray10"
+                      :inherit 'mode-line-inactive)
+  (set-face-attribute 'perspeen-tab--powerline-inactive1 nil
+                      :background "gray10"
+                      :inherit 'mode-line-inactive))
+
+
 
 ;;; core.el ends here
