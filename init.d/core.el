a99acb86922086f1f6e176a9c189e71e11081df0
kurubushi <krbshi@gmail.com>
2020-09-07 00:24:12 +0900

    [emacs] add telephone-line

--- init.d/core.el
+++ init.d/core.el
@@ -631,4 +631,45 @@ http://emacsredux.com/blog/2013/06/21/eval-and-replace/"
 
 
 
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+; nyan-mode
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(use-package nyan-mode
+  :el-get nyan-mode
+  :config (nyan-mode t))
+
+
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+; telephone-line
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(use-package telephone-line
+  :el-get telephone-line
+  :config
+  ;; https://github.com/dbordak/telephone-line/blob/master/examples.org
+  (setq telephone-line-evil-use-short-tag t)
+  (telephone-line-defsegment telephone-line-perspeen-segment ()
+    (when perspeen-mode
+      (format "[%s]" (perspeen-ws-struct-name perspeen-current-ws))))
+  (telephone-line-defsegment telephone-line-mule-info-segment ()
+    '("" mode-line-mule-info "%*"))
+
+  (setq telephone-line-lhs
+        '((evil   . (telephone-line-evil-tag-segment))
+          (accent . (telephone-line-major-mode-segment
+                     telephone-line-mule-info-segment))
+          (nil    . (telephone-line-buffer-name-segment))))
+  (setq telephone-line-center-rhs
+        '((accent . (telephone-line-process-segment
+                     telephone-line-airline-position-segment))
+          (nil    . (telephone-line-nyan-segment))))
+  (setq telephone-line-rhs
+        '((accent  . (telephone-line-vc-segment
+                      telephone-line-perspeen-segment))))
+
+  (telephone-line-mode t))
+
 ;;; core.el ends here
