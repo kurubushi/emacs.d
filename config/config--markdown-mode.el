c6e7579093e9dde9337110e48314b6a53506f9ad
kurubushi <krbshi@gmail.com>
2021-06-21 00:50:53 +0900

    :sparkles: [markdown] 見栄え変更

--- config/config--markdown-mode.el
+++ config/config--markdown-mode.el
@@ -14,7 +14,14 @@
 
 (use-package markdown-mode
   :el-get markdown-mode
-  :mode (("\\.md\\'" . markdown-mode)))
+  :mode (("\\.md\\'" . markdown-mode))
+  :custom
+  (markdown-header-scaling t)
+  (markdown-header-scaling-values '(1.6 1.4 1.2 1.0 1.0 1.0))
+  (markdown-hide-urls t)
+  (markdown-make-gfm-checkboxes-buttons t)
+  (markdown-enable-math t)
+  (markdown-display-remote-images t))
 
 
 (provide 'config--markdown-mode)
