7f318abec3bac9472ba648d8d9ef394adfd89403
kurubushi <krbshi@gmail.com>
2021-07-20 11:25:40 +0900

    :sparkles: add emojify

---
+++ config/config--emojify.el
@@ -0,0 +1,26 @@
+;;; config--emojify.el --- Configuration of emojify.
+
+;;; Commentary:
+
+;;; Code:
+
+(eval-when-compile
+  (add-to-list 'load-path (concat user-emacs-directory "el-get/use-package")))
+
+(require 'use-package)
+
+
+;;; use-package
+
+;; GitHub: https://github.com/iqbalansari/emacs-emojify
+
+(use-package emojify
+  :el-get emojify
+  :hook (after-init . global-emojify-mode)
+  :config (emojify-set-emoji-styles '(github)))
+
+
+(provide 'config--emojify)
+
+
+;;; config--emojify.el ends here
--- init.el
+++ init.el
@@ -90,6 +90,7 @@
 ;;; view
 
 (use-package config--all-the-icons)
+(use-package config--emojify)
 (use-package config--nyan-mode)
 (use-package config--doom-themes)
 (use-package config--doom-modeline)
