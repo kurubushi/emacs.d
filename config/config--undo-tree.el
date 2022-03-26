90e8f44eee7e05520410357b6381f26c38f2b301
kurubushi <krbshi@gmail.com>
2022-03-26 22:30:09 +0900

    :sparkles: avoid creation undo-tree cache files

--- config/config--undo-tree.el
+++ config/config--undo-tree.el
@@ -7,7 +7,16 @@
 (require 'use-package)
 (require 'quelpa-use-package)
 
+(defvar undo-tree-directory
+  (concat user-emacs-directory "undo-tree/")
+  "Directory for undo-tree.")
+
+(defvar undo-tree-history-directory
+  (concat undo-tree-directory "histories/")
+  "Directory for undo-tree histories.")
+
 (use-package undo-tree
+  :custom (undo-tree-history-directory-alist `(("." . ,undo-tree-history-directory)))
   :quelpa (undo-tree :fetcher git
 		     :url "git@gitlab.com:tsc25/undo-tree.git")
   :config (global-undo-tree-mode))
