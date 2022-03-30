c65415acbd86983042b823a1cd14c8f2535e9488
kurubushi <krbshi@gmail.com>
2022-03-31 03:03:12 +0900

    :fire: C-o 問題なくなった気がするので対応消し
    C-o しても evil-move-cursor-back は t から nil に書き換えられなくなっ
    てた。

--- config/config--evil.el
+++ config/config--evil.el
@@ -24,20 +24,7 @@
 
   ;; https://github.com/emacs-evil/evil/pull/1360
   (evil-set-undo-system 'undo-tree)
-  (evil-mode 1)
-
-  ;;keymap
-  ;; original `evil-execute-in-normal-state` executes as "C-\ C-o" of Vim.
-  ;; `evil-execute-in-normal-state-natively` executes as "C-o" of Vim.
-  (defun evil-execute-in-normal-state-natively ()
-    "Execute the next command in Normal state, natively."
-    (interactive)
-    (setq evil-move-cursor-back_ evil-move-cursor-back)
-    (evil-execute-in-normal-state)
-    ;; returns to original value without wating evil-delay
-    (setq evil-move-cursor-back evil-move-cursor-back_))
-  (general-define-key :keymaps '(insert)
-                      "C-o" 'evil-execute-in-normal-state-natively))
+  (evil-mode 1))
 
 (provide 'config--evil)
 
