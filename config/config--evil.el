553b3e4ebf1b1b828f7193f27d543a6e1f9c807d
kurubushi <krbshi@gmail.com>
2022-03-31 03:09:03 +0900

    :recycle: evil の defcustom を整理
    たぶん :custom で問題ないはず。

--- config/config--evil.el
+++ config/config--evil.el
@@ -12,18 +12,21 @@
   :quelpa
   :after config--undo-tree
 
-  :init
-  (custom-set-variables '(evil-want-keybinding nil)) ; for evil-collection
+  :custom
+  (evil-want-C-u-scroll t)
+  (evil-want-visual-char-semi-exclusive t) ; exclusive \n in visual state
+  (evil-search-module 'isearch)
+
+  ;; evil-collection assumes the following values.
+  ;; ref. https://github.com/emacs-evil/evil-collection
+  (evil-want-keybinding nil)
+  (evil-want-integration t)
+
+  ;; https://github.com/syl20bnr/spacemacs/issues/3623
+  (search-invisible t)
 
   :config
-  (custom-set-variables '(search-invisible t)) ;https://github.com/syl20bnr/spacemacs/issues/3623
-  (custom-set-variables '(evil-want-C-u-scroll t))
-  (custom-set-variables  '(evil-want-visual-char-semi-exclusive t)) ; exclusive \n in visual state
-  (custom-set-variables '(evil-want-integration nil)) ; for evil-collection
-  (custom-set-variables  '(evil-search-module 'isearch))
-
-  ;; https://github.com/emacs-evil/evil/pull/1360
-  (evil-set-undo-system 'undo-tree)
+  (evil-set-undo-system 'undo-tree) ; https://github.com/emacs-evil/evil/pull/1360
   (evil-mode 1))
 
 (provide 'config--evil)
