6b4d50ae60cebe00badbc4e089b11ac53371f6f6
kurubushi <krbshi@gmail.com>
2022-04-06 14:45:58 +0900

    :recycle: web-mode の設定整理

--- config/config--web-mode.el
+++ config/config--web-mode.el
@@ -7,26 +7,30 @@
 (require 'use-package)
 (require 'quelpa-use-package)
 
-(use-package add-node-modules-path
-  :quelpa)
-
-(use-package prettier-js
-  :quelpa)
-
 (use-package web-mode
   :quelpa
-  :after config--flycheck
+  :after flycheck
+  :custom
+  (web-mode-markup-indent-offset 2)
+  (web-mode-css-indent-offset 2)
+  (web-mode-code-indent-offset 2)
+
   :mode (("\\.js\\'" . web-mode)
          ("\\.ts\\'" . web-mode)
          ("\\.tsx\\'" . web-mode))
-  :init
-  (setq web-mode-markup-indent-offset 2)
-  (setq web-mode-css-indent-offset 2)
-  (setq web-mode-code-indent-offset 2)
+
   :config
-  (flycheck-add-mode 'javascript-eslint 'web-mode)
-  (add-hook 'web-mode-hook 'add-node-modules-path)
-  (add-hook 'web-mode-hook 'prettier-js-mode))
+  (flycheck-add-mode 'javascript-eslint 'web-mode))
+
+(use-package add-node-modules-path
+  :quelpa
+  :after web-mode
+  :hook (web-mode . add-node-modules-path))
+
+(use-package prettier-js
+  :quelpa
+  :after web-mode
+  :hook (web-mode . prettier-js-mode))
 
 (provide 'config--web-mode)
 
