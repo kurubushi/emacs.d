d99671ca4c8a9b7612465434518bcd015cff2a16
kurubushi <krbshi@gmail.com>
2022-04-10 11:36:27 +0900

    :recycle: skk-mode を use-package keyword 使って整理

--- config/config--skk.el
+++ config/config--skk.el
@@ -6,42 +6,45 @@
 
 (require 'use-package)
 (require 'quelpa-use-package)
+(require 'general)
 
 ;; https://skk-dev.github.io/ddskk/skk.html
 
 (use-package skk
   :quelpa ddskk
-  :init
-  (setq skk-sticky-key ";")
-  (setq skk-kutouten-type 'jp)
-  (setq skk-rom-kana-rule-list
-                '(("!" nil "！")
-                  ("?" nil "？")))
-  ;;(setq skk-large-jisyo "/large/jisyo/path") ; setq in prefix.el
+  :custom
+  (skk-sticky-key ";")
+  (skk-kutouten-type 'jp)
+  (skk-rom-kana-rule-list '(("!" nil "！")
+                            ("?" nil "？")))
+  ;;(skk-large-jisyo "/large/jisyo/path") ; in prefix.el
 
   :config
-  ;; to make global skk minor mode and use it always
-  ;; ref: https://stackoverflow.com/questions/16048231/
-  (define-globalized-minor-mode global-skk-mode skk-mode
-    (lambda () (skk-latin-mode 1)))
-  (global-skk-mode)
-
-  ;; to set C-j to evil-ex state
-  ;; ref: https://emacs.stackexchange.com/questions/14163/
-  (general-define-key :keymaps '(evil-ex-completion-map)
-                      "C-j" 'skk-kakutei)
-
-  ;; to return skk-latin-mode when entrying/exiting from insert-state
-  (add-hook 'evil-insert-state-entry-hook 'skk-latin-mode-on)
-  (add-hook 'evil-insert-state-exit-hook 'skk-latin-mode-on)
-
   ;; priority to skk-j-mode-map over any minor mode map
   ;; as default, haskell-mode binds ";" that I want to be 'skk-kakutei
   ;; ref: https://github.com/haskell/haskell-mode/issues/1320
   (defun skk-j-overrideing-minor ()
     (add-to-list 'minor-mode-overriding-map-alist
                  `(skk-j-mode . ,skk-j-mode-map)))
-  (add-hook 'skk-mode-hook 'skk-j-overrideing-minor))
+
+  ;; to make global skk minor mode and use it always
+  ;; ref: https://stackoverflow.com/questions/16048231/
+  (define-globalized-minor-mode global-skk-mode skk-mode
+    (lambda () (skk-latin-mode 1)))
+
+  (global-skk-mode)
+
+  :general
+  ;; to set C-j to evil-ex state
+  ;; ref: https://emacs.stackexchange.com/questions/14163/
+  (general-define-key :keymaps '(evil-ex-completion-map)
+                      "C-j" 'skk-kakutei)
+
+  :hook
+  ((skk-mode . skk-j-overrideing-minor)
+   ;; to return skk-latin-mode when entrying/exiting from insert-state
+   (evil-insert-state-entry . skk-latin-mode-on)
+   (evil-insert-state-exit  . skk-latin-mode-on)))
 
 (provide 'config--skk)
 
