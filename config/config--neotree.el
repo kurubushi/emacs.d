e4301ebf3d4475f5c06dde8c74ee7d5911d936c1
kurubushi <krbshi@gmail.com>
2022-04-25 22:35:56 +0900

    :sparkles: neotree buffer は単に開き閉じするだけ

--- config/config--neotree.el
+++ config/config--neotree.el
@@ -12,26 +12,35 @@
   :quelpa
   :after (config--evil)
   :commands (neotree-show neotree-hide neotree-dir neotree-find)
-  :custom
-  (neo-theme 'nerd2)
+  :custom (neo-theme 'nerd2)
+
   :config
   (evil-set-initial-state 'neotree-mode 'emacs)
-  (defun neotree-toggle-on-current-buffer ()
+
+  (defun neotree-find-current-buffer ()
     "Open neotree buffer with the current buffer."
     (interactive)
-    (if (neo-global--window-exists-p)
-        (neotree-hide)
-      (let ((filepath (buffer-file-name)))
-        (neotree-find filepath))))
+    (let ((filepath (buffer-file-name)))
+      (neotree-find filepath)))
+
   (defun neotree-go-up-node ()
     "Go to parent of selected node."
     (interactive)
     (goto-char (point-min))
     (neotree-select-up-node))
+
+  (defun neotree-toggle ()
+    "Open and close the side bar for neotree."
+    (interactive)
+    (if (neo-global--window-exists-p)
+        (neotree-hide)
+      (neotree-show)))
+
   :general
   (general-define-key :keymaps 'normal
                       :prefix "SPC f"
-                      "d" 'neotree-toggle-on-current-buffer)
+                      "d" 'neotree-toggle
+                      "D" 'neotree-find-current-buffer)
   (general-define-key :keymaps 'neotree-mode-map
                       :states 'normal
                       "j" 'neotree-next-line
