37a6a6576c40fe7f801ec14483755c37cd29b87c
kurubushi <krbshi@gmail.com>
2023-04-23 20:16:07 +0900

    :bug: exclude a next line in the visual mode

--- config/config--evil.el
+++ config/config--evil.el
@@ -16,6 +16,23 @@
   (evil-want-C-u-scroll t)
   (evil-search-module 'isearch)
 
+  ;; `evil-want-visual-char-semi-exclusive' is deprecated
+  ;; and `evil-v$-excludes-newline' is recommended,
+  ;; but I dare to use `evil-want-visual-char-semi-exclusive'.
+  ;; Only with `evil-want-visual-char-semi-exclusive',
+  ;; the visual mode can select an end of line,
+  ;; and the next line is contained in selected range.
+  ;; For example, consider the following text:
+  ;; -----
+  ;; abc
+  ;;
+  ;; def
+  ;; -----
+  ;; Only with `evil-want-visual-char-semi-exclusive',
+  ;; the command `vjD' at the beginning of the text
+  ;; deletes the `def' line against my will.
+  (evil-want-visual-char-semi-exclusive t)
+
   ;; evil-collection assumes the following values.
   ;; ref. https://github.com/emacs-evil/evil-collection
   (evil-want-keybinding nil)
