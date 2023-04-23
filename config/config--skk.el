e57af12f423d10a0813ad7c4753bb6ea4308a7d8
kurubushi <krbshi@gmail.com>
2023-04-24 02:25:55 +0900

    :sparkles: refer to local SKK-JISYOes installed in ~/.skk.d

--- config/config--skk.el
+++ config/config--skk.el
@@ -8,19 +8,40 @@
 (require 'quelpa-use-package)
 (require 'general)
 
+(defun get-jisyo-path (jisyo)
+  "Get SKK-JISYO file path of JISYO."
+  (if (stringp jisyo)
+      jisyo
+    (car jisyo)))
+
+(defun available-jisyo-p (jisyo)
+  "Check if JISYO is available."
+  (file-readable-p (get-jisyo-path jisyo)))
+
+(defun home/ (path)
+  "Prepend `$HOME' to PATH."
+  (format "%s/%s" (getenv "HOME") path))
+
 ;; https://skk-dev.github.io/ddskk/skk.html
+
+;; install JISYO
+;; - ~/.skk.d/SKK-JISYO.L (symbolic link is OK)
+;; - ~/.skk.d/SKK-JISYO.*.utf8
 
 (use-package skk
   :quelpa ddskk
   :custom
+  (skk-large-jisyo (home/ ".skk.d/SKK-JISYO.L"))
   (skk-jisyo `(,(concat user-emacs-directory "skk-jisyo.utf8") . utf-8))
   ;; Each file name (excluding directory path) must not conflict with others.
   ;; For example, let `skk-jisyo' be "~/.emacs.d/skk-jisyo";
   ;; even if `skk-extra-jisyo-file-list' contains "/other/directory/path/skk-jisyo",
   ;; the jisyo is not a search target.
-  ;; Extra jisyo pathes are defined at post-config.el.
-  ;;(setq skk-large-jisyo "/large/jisyo/path")
-  ;;(setq skk-extra-jisyo-file-list '("/jisyo/path" ("/jisyo/path.utf8" . utf-8)))
+  (skk-extra-jisyo-file-list
+   (seq-filter #'available-jisyo-p
+               `((,(home/ ".skk.d/SKK-JISYO.user.utf8") . utf-8)
+                 (,(home/ ".skk.d/SKK-JISYO.emoji.utf8") . utf-8)
+                 (,(home/ ".skk.d/SKK-JISYO.jawiki.utf8") . utf-8))))
   (skk-sticky-key ";")
   (skk-kutouten-type 'jp)
   (skk-rom-kana-rule-list '(("!" nil "！")
