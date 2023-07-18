a15cae766aa89f2e1269f40c01c6b86e16490949
kurubushi <krbshi@gmail.com>
2023-07-18 23:58:26 +0900

    :sparkles: execute stree write on save

--- config/config--ruby-mode.el
+++ config/config--ruby-mode.el
@@ -10,7 +10,42 @@
 (use-package ruby-mode
   :quelpa
   :mode ("\\.rb\\'" . ruby-mode)
-  :config (setq ruby-insert-encoding-magic-comment nil))
+
+  :config
+  (setq ruby-insert-encoding-magic-comment nil)
+
+  ;; If you want stree format the buffer on save,
+  ;; add the following to your .dir-locals.el:
+  ;;
+  ;; ((ruby-mode . ((stree-write-on-save . t))))
+  ;;
+  (defvar stree-write-on-save nil
+    "If non-nil, execute stree write command on save.")
+
+  (defmacro with-cd (directory &rest body)
+    "Execute BODY with changing directory to DIRECTORY temporarily"
+    `(with-temp-buffer
+       (cd ,directory)
+       ,@body))
+
+  (defun execute-stree-write (buffer)
+    "Execute stree write command at the BUFFER."
+    (let* ((file-path (buffer-file-name buffer))
+           (root-path (locate-dominating-file (file-name-directory file-path) ".streerc"))
+           (dir-path (or root-path (file-name-directory file-path))))
+      (message "Formatting %s." file-path)
+      (with-cd dir-path (shell-command (format "stree write %s" file-path)))
+      (message "Formatted %s." file-path)
+      (with-current-buffer buffer (revert-buffer t t t))))
+
+  (defun execute-stree-write-on-save ()
+    "If `stree-write-on-save' is not `nil', execute stree write command on save."
+    (when stree-write-on-save
+      (message (format "Executing at %s" (buffer-file-name)))
+      (execute-stree-write (current-buffer))))
+
+  :hook
+  (after-save . execute-stree-write-on-save))
 
 (provide 'config--ruby-mode)
 
