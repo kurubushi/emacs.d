605e72407a1b79b75538052a8297556b4e3a3bce
kurubushi <krbshi@gmail.com>
2023-07-23 00:25:16 +0900

    :recycle: enable to replace font-family

--- config/config--emacs.el
+++ config/config--emacs.el
@@ -24,17 +24,22 @@
 
 ;;; fonts
 
-;; depends on Ricty and Noto Color Emoji
-;;   $ yay -S noto-fonts-emoji ttf-ricty
+;; depends on Ricty and Noto Color Emoji fonts.
+;;   $ yay -S ttf-ricty noto-fonts-emoji
+;; zenkaku-space 　 is displayed with Ricty and
+;; :smile: is displayed with Noto.
+(defconst default-font-family "Ricty")
+(defconst default-emoji-font-family "Noto Color Emoji")
+(defconst default-font-size 14)
 
 (defun set-my-font-with-size (size)
   "Set my font in the SIZE."
   (interactive "nsize: ")
-  (set-face-attribute 'default nil :family "Ricty" :height (* size 10))
-  (set-fontset-font "fontset-default" 'unicode (font-spec :family "Ricty"))
+  (set-face-attribute 'default nil :family default-font-family :height (* size 10))
+  (set-fontset-font "fontset-default" 'unicode (font-spec :family default-font-family))
   ;; 'symbol を選択しても :wrench: に Noto が使われなかったので 'unicode を選択する
   ;; 上の Ricty より優先されるように 'prepend を指定する
-  (set-fontset-font "fontset-default" 'unicode (font-spec :family "Noto Color Emoji") nil 'prepend))
+  (set-fontset-font "fontset-default" 'unicode (font-spec :family default-emoji-font-family) nil 'prepend))
 
 (set-my-font-with-size 15)
 
