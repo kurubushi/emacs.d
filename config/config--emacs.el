be33f14dc2b51aea9d7436cb2d0b48fa45600a87
kurubushi <krbshi@gmail.com>
2023-07-25 03:57:38 +0900

    :lipstick: use Ricty Diminished

--- config/config--emacs.el
+++ config/config--emacs.el
@@ -26,11 +26,11 @@
 
 ;;; fonts
 
-;; depends on Ricty and Noto Color Emoji fonts.
-;;   $ yay -S ttf-ricty noto-fonts-emoji
-;; zenkaku-space 　 is displayed with Ricty and
+;; depends on Ricty Diminished and Noto Color Emoji fonts.
+;;   $ yay -S ttf-ricty-diminished noto-fonts-emoji
+;; zenkaku-space 　 is displayed with Ricty Diminished and
 ;; :smile: is displayed with Noto.
-(defconst default-font-family "Ricty")
+(defconst default-font-family "Ricty Diminished")
 (defconst default-emoji-font-family "Noto Color Emoji")
 (defconst default-font-size 14)
 
@@ -41,7 +41,9 @@
   (interactive (list (completing-read "font-family: " (font-family-list) nil t default-font-family)))
   (if (not (installed-font-family-p font-family))
       (warn "font-family \"%s\" is not installed" font-family)
-    (set-fontset-font t 'unicode (font-spec :family font-family)))
+    (set-fontset-font t 'ascii (font-spec :family font-family))
+    (set-fontset-font t 'cp932 (font-spec :family font-family))
+    (set-fontset-font t 'symbol (font-spec :family font-family)))
   (if (not (installed-font-family-p default-emoji-font-family))
       (warn "font-family \"%s\" is not installed" default-emoji-font-family)
     (set-fontset-font t 'unicode (font-spec :family default-emoji-font-family) nil 'prepend)))
