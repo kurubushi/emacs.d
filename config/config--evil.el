2dbd6091182ac5c10343c748bc99972784a717b6
kurubushi <krbshi@gmail.com>
2024-12-25 19:05:21 +0900

    :fire: remove workaround fixing visual mode

--- config/config--evil.el
+++ config/config--evil.el
@@ -32,50 +32,6 @@
   ;; https://github.com/emacs-evil/evil/pull/1360
   (evil-set-undo-system 'undo-tree)
 
-  ;; FIXME: Neither `evil-want-visual-char-semi-exclusive' nor
-  ;; `evil-v$-excludes-newline' can simulate the visual mode of vi.
-  ;;
-  ;; For example, consider the following text:
-  ;;
-  ;; -----
-  ;; ab
-  ;;
-  ;; cd
-  ;; -----
-  ;;
-  ;; When `evil-want-visual-char-semi-exclusive' is not nil,
-  ;; `lvjD' is expected to delete `ab' and the empty line;
-  ;; however, the command cannot delete `a' and
-  ;; the result is:
-  ;;
-  ;; -----
-  ;; a
-  ;; cd
-  ;; -----
-  ;;
-  ;; When `evil-v$-excludes-newline' is not nil,
-  ;; `vjD' is expected to delete only `ab' and the empty line;
-  ;; however, the command deletes also `cd' line and
-  ;; the result is:
-  ;;
-  ;; -----
-  ;; -----
-  ;;
-  ;; A workaround: both `Vjd' and `VjD' work well.
-  ;;
-  ;; Rewrite `evil-*-line commands':
-  (defun evil-*-line-with-1-end (args)
-    (if (not (evil-visual-state-p)) args
-      (pcase args
-        (`(,beg ,end . ,rest) (append `(,beg ,(1- end)) rest)))))
-
-  (mapc (lambda (f)
-          (advice-add f :filter-args 'evil-*-line-with-1-end))
-        '(evil-delete-line
-          evil-yank-line
-          evil-change-line
-          evil-indent-line))
-
   (evil-mode 1))
 
 (provide 'config--evil)
